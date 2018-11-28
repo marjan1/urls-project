25c9fe6696414ca0515e0077bc48ce8a79eaa7a7
marjan <marjan.ilievski@gmail.com>
2018-11-28 21:14:28 +0100

    init commit 2

---
+++ backend/src/main/java/com/eadvocate/ELawyerApplication.java
@@ -0,0 +1,67 @@
+package com.eadvocate;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcRegistrations;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.mvc.condition.PatternsRequestCondition;
+import org.springframework.web.servlet.mvc.method.RequestMappingInfo;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+
+import java.lang.reflect.Method;
+
+@SpringBootApplication
+@EnableAdminServer
+public class ELawyerApplication {
+
+    @Value("${rest.api.base.path}")
+    private String restApiBasePath;
+
+    public static void main(String[] args) {
+        SpringApplication.run(ELawyerApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("*").allowedOrigins("http://localhost:4200");
+            }
+        };
+    }
+
+    @Bean
+    public WebMvcRegistrations webMvcRegistrationsHandlerMapping() {
+        ELawyerApplication application = this;
+        return new WebMvcRegistrations() {
+            @Override
+            public RequestMappingHandlerMapping getRequestMappingHandlerMapping() {
+                return new RequestMappingHandlerMapping() {
+
+                    @Override
+                    protected void registerHandlerMethod(Object handler, Method method, RequestMappingInfo mapping) {
+                        Class<?> beanType = method.getDeclaringClass();
+                        RestController restApiController = beanType.getAnnotation(RestController.class);
+                        if (restApiController != null) {
+                            PatternsRequestCondition apiPattern = new PatternsRequestCondition(application.restApiBasePath)
+                                    .combine(mapping.getPatternsCondition());
+
+                            mapping = new RequestMappingInfo(mapping.getName(), apiPattern,
+                                    mapping.getMethodsCondition(), mapping.getParamsCondition(),
+                                    mapping.getHeadersCondition(), mapping.getConsumesCondition(),
+                                    mapping.getProducesCondition(), mapping.getCustomCondition());
+                        }
+
+                        super.registerHandlerMethod(handler, method, mapping);
+                    }
+                };
+            }
+        };
+    }
+}
---
+++ backend/src/test/java/com/eadvocate/ELawyerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.eadvocate;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ELawyerApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
